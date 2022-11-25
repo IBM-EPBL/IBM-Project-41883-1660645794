c71da5efef6ae16740531be51fcfac38e57a8b88
Malar08 <113434685+Malar08112001@users.noreply.github.com>
2022-11-25 20:11:20 +0530

    Delete app.py

--- Final Deliverables/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-#app=Flask(__name__)
-app=Flask(__name__,template_folder='template')
-@app.route('/')
-def bot():
-    return render_template('Chatbot.html')
-if __name__ =='__main__':
-    app.debug = True
-    app.run()
